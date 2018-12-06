5551be96b239237c0a8b8ce21f3d0d46a1d7071a
Dylan Hofmann <dylan.hofmann@gmail.com>
2018-12-06 15:06:54 -0700

    Finish file set up and slight refactor of players creation

---
+++ test/Wheel-test.js
@@ -0,0 +1,14 @@
+const chai = require('chai');
+const spies = require('chai-spies');
+chai.use(spies);
+const expect = chai.expect;
+const Wheel = require('../lib/Wheel.js');
+global.domUpdates = require('../domUpdates.js');
+
+//chai.spy.on(global.domUpdates, ['methods here'], () => true);
+
+describe('Wheel', function() {
+
+
+  
+})
