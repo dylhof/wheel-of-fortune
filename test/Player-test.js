592c5959b9ad14e5c66602ac2d6980c10722fb6a
Shannon Moranetz <shannonrmoranetz@gmail.com>
2018-12-06 17:02:12 -0700

    Add initial player tests

--- test/Player-test.js
+++ test/Player-test.js
@@ -8,7 +8,33 @@ global.domUpdates = require('../domUpdates.js');
 //chai.spy.on(global.domUpdates, ['methods here'], () => true);
 
 describe('Player', function() {
+  var player;
+  beforeEach(function() {
+    player = new Player('Susan');
+  });
 
+  it('should have a name', function() {
+    expect(player.playerName).to.equal('Susan');
+  });
 
-  
-})
+  it.skip('should reset the round score', function() {
+    player.resetRoundScore();
+    expect(player.roundScore).to.equal(0);
+  });
+
+    it.skip('should subtract $100 from round score', function() {
+    player.purchaseVowel();
+    expect(player.roundScore).to.equal();
+  });
+
+    it.skip('should add the round score to the total score', function() {
+    player.sumTotalScore();
+    expect(player.totalScore).to.equal();
+  });
+
+    it.skip('should toggle isTurn property boolean value', function() {
+    expect(player.isTurn).to.equal(false);
+    player.updateTurn();
+    expect(player.isTurn).to.equal(true);
+  });
+});
