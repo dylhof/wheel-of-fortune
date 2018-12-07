ee2bdedd8b1465963f2218762e1c5a2cc47edc67
Dylan Hofmann <dylan.hofmann@gmail.com>
2018-12-06 18:19:10 -0700

    Add initial wheel tests

--- test/Wheel-test.js
+++ test/Wheel-test.js
@@ -4,11 +4,29 @@ chai.use(spies);
 const expect = chai.expect;
 const Wheel = require('../lib/Wheel.js');
 global.domUpdates = require('../domUpdates.js');
+const data = require('../lib/data.js')
 
 //chai.spy.on(global.domUpdates, ['methods here'], () => true);
 
 describe('Wheel', function() {
+  var wheel;
+  beforeEach(function() {
+    wheel = new Wheel();
+  });
 
+  it('should have an empty array', function() {
+    expect(wheel.wheelValues).to.deep.equal([]);
+  });
 
+  it.skip('should have an array of wheel values', function() {
+    wheel.generateWheelValues()
+    expect(wheel.wheelValues.length).to.equal(6);
+  });
+
+  it.skip('should choose a random value', function() {
+    wheel.spin()
+    expect().to.equal();
+    //Maybe spy on this to see if it is triggering a dom update? 
+  });
   
 })
