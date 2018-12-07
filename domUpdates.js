6d9ebd8eda61b91e8ded5a5e1165284219f1839d
Dylan Hofmann <dylan.hofmann@gmail.com>
2018-12-06 19:13:41 -0700

    Add initial dom manipulation functions to domUpdates

--- domUpdates.js
+++ domUpdates.js
@@ -1,3 +1,14 @@
-const DomUpdates = {
-  
+const domUpdates = {
+  updatePuzzleBoardOnDom(currentPuzzle) {
+    $('.puzzle-board').text(currentPuzzle.correct_answer)
+  },
+
+  updatePuzzleCategoryOnDom(currentPuzzle){
+    currentPuzzle.category
+  },
+
+  updateSpinValueOnDom(spinValue) {
+    $('.spin-value').text(spinValue)
+  }
+
 }
